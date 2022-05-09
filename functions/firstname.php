63b93b18a06d88f15572b154f4b5f2b4a3cd7c3a
ChocoIa <steevendepro@gmail.com>
2022-05-09 15:25:40 +0200

    first commit

---
+++ functions/firstname.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Steeven;
+
+function checkPassword($password) {
+
+    echo '<h1 class="text-center">Steeven</h1>';
+
+    // ...
+
+}
---
+++ functions/steeven.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace steeven;
+
+function checkPassword($password) {
+
+    echo '<h1 class="text-center">Steeven</h1>';
+
+    // ...
+
+}
